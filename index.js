b29d0b2285ccc3422e5c85e58db9ab94bc368b53
Samantha Malone <sammac99@gmail.com>
2021-07-01 14:28:27 -0700

    initializing folders and files

---
+++ index.js
@@ -0,0 +1,5 @@
+// require 3 class constructors
+// need Inquirer, Path, fs
+// require index.js in src folder
+
+// 5 functions (createManager, createEngineer, createIntern, call as last thing in prev. functions: askNext: asks What do you want to do next--recursion, See results when ready to quit: new function calls fs function...within functions add details)
---
+++ src/index.js
@@ -0,0 +1,1 @@
+// creates html for employees like markdown....need a loop for # of employees, so need an array in other index.js
