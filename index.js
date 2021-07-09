1c1e8cb4de45ce2b3d30e48ed35e0fc809760d4e
Samantha Malone <sammac99@gmail.com>
2021-07-09 13:59:38 -0700

    added intern and manager const fun

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // require 3 class constructors
 // need Inquirer, Path, fs
 // require index.js in src folder
-const Inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const inquirer = require("inquirer");
+
 
 const employees = [];
 
