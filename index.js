b027a8a3132d668865ac6a233f959b72598245a9
Samantha Malone <sammac99@gmail.com>
2021-07-11 17:23:29 -0700

    refactoring page templte and html write file in office hours with Raj

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+const genHtml = require("./src/page-template");
 // require 3 class constructors
 // need Inquirer, Path, fs
 // require index.js in src folder
 const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require("path");
-const genHTML = require("./src/page-template");
+// const path = require("path");
+// const addHtml = require("./src/page-template.js");
+
+// const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+// const OUT_DIR = path.resolve(__dirname, "dist");
+// const createPath = path.join(OUT_DIR, "roster.html");
+
 const employees = [];
 
+// function init() {
+//   genHtml();
+//   addEmployee();
+// }
+
 // 5 functions (createManager, createEngineer, createIntern, call as last thing in prev. functions: askNext: asks What do you want to do next--recursion, See results when ready to quit: new function calls fs function...within functions add details)
-function addEmployee() {
-  inquirer
-    .prompt([
+const addMan= () => {
+ return inquirer.prompt([
       {
-        message: "Enter employee's name",
+        type: "input",
+        message: "Enter manager's name",
         name: "name",
         validate: (addInput) => {
           if (addInput) {
@@ -28,20 +39,7 @@ function addEmployee() {
       },
     },
       {
-        type: "list",
-        message: "What is their role?",
-        choices: ["Manager", "Engineer", "Intern"],
-        name: "role",
-        validate: (addInput) => {
-          if (addInput) {
-          return true;
-        } else {
-          console.log("Invalid");
-          return false;
-        }
-      },
-      },
-      {
+        type: "input",
         message: "What is their ID?",
         name: "id",
         validate: (addInput) => {
@@ -54,6 +52,7 @@ function addEmployee() {
       },
       },
       {
+        type: "input",
         message: "What is their email?",
         name: "email",
         validate: (addInput) => {
@@ -65,119 +64,107 @@ function addEmployee() {
         }
       },
       },
+      {
+        type: "input",
+        message: "What is their work phone number?",
+        name: "work number",
+        validate: (addInput) => {
+          if (addInput) {
+          return true;
+        } else {
+          console.log("Invalid");
+          return false;
+        }
+      },
+      },
     ])
-    .then(function ({ name, role, id, email }) {
-      let roleDetail = "";
-      if (role === "Intern") {
-        roleDetail = "school name";
-      } else if (role === "Engineer") {
-        roleDetail = "Github name";
-      } else {
-        roleDetail = "work number";
-      }
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: `Enter employee's ${roleDetail}`,
-            name: "roleDetail",
-          },
-          {
-            type: "list",
-            message: "Would you like to continue adding employees?",
-            choices: ["yes", "no"],
-            name: "addMoreEmployees",
-          },
-        ])
-        .then(function ({ roleDetail, addMoreEmployees }) {
-          let newEmployee;
-          if (role === "Engineer") {
-            newEmployee = new Engineer(name, id, email, roleDetail);
-          } else if (role === "Intern") {
-            newEmployee = new Intern(name, id, email, roleDetail);
-          } else {
-            newEmployee = new Manager(name, id, email, roleDetail);
-          }
-          employees.push(newEmployee);
-          addHtml(newEmployee).then(function () {
-            if (addMoreEmployees === "yes") {
-              addMoreEmployees();
-            } else {
-              finishHtml();
-            }
-          });
-        });
-    });
-};
+    // .then(function ({ name, role, id, email }) {
+    //   let roleDetail = "";
+    //   if (role === "Intern") {
+    //     roleDetail = "school name";
+    //   } else if (role === "Engineer") {
+    //     roleDetail = "Github name";
+    //   } else {
+    //     roleDetail = "work number";
+    //   }
+    //   inquirer
+    //     .prompt([
+    //       {
+    //         type: "input",
+    //         message: `Enter employee's ${roleDetail}`,
+    //         name: "roleDetail",
+    //       },
+    //       {
+    //         type: "list",
+    //         message: "Would you like to continue adding employees?",
+    //         choices: ["yes", "no"],
+    //         name: "addMoreEmployees",
+    //       },
+    //     ])
+    //     .then(function ({ roleDetail, addMoreEmployees }) {
+    //       let newEmployee;
+    //       if (role === "Engineer") {
+    //         newEmployee = new Engineer(name, id, email, roleDetail);
+    //       } else if (role === "Intern") {
+    //         newEmployee = new Intern(name, id, email, roleDetail);
+    //       } else {
+    //         newEmployee = new Manager(name, id, email, roleDetail);
+    //       }
+    //       employees.push(newEmployee);
+    //       addHtml(newEmployee).then(function () {
+    //         if (addMoreEmployees === "yes") {
+    //           addMoreEmployees();
+    //         } else {
+    //           finishHtml();
+    //         }
+    //       });
+    //     });
+    // });
+}
 
-function addHtml(arr) {
-  let html = ""
-  arr.forEach((employee) => {
-    const name = employee.getName();
-    const role = employee.getRole();
-    const id = employee.getId();
-    const email = employee.getEmail();
-    if (role === "Engineer") {
-      const gitHub = employee.getGitHub();
-      html += `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${id}</li>
-            <li class="list-group-item">Email Address: ${email}</li>
-            <li class="list-group-item">GitHub: ${gitHub}</li>
-            </ul>
-            </div>
-        </div>`;
-    } else if (role === "Intern") {
-      const school = employee.getSchool();
-      html += `<div class="col-6">
-        <div class="card mx-auto mb-3" style="width: 18rem">
-        <h5 class="card-header">${name}<br /><br />Intern</h5>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${id}</li>
-            <li class="list-group-item">Email Address: ${email}</li>
-            <li class="list-group-item">School: ${school}</li>
-            </ul>
-            </div>
-            </div>`;
-    } else {
-      const workNumber = employee.getWorkNumber();
-      html += `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Manager</h5>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${id}</li>
-            <li class="list-group-item">Email Address: ${email}</li>
-            <li class="list-group-item">Office Phone: ${workNumber}</li>
-            </ul>
-            </div>
-            </div>`;
-    }
-  })
 
-  console.log("adding employees");
-  html += 
-
+console.log("adding employees");
+  // html +=
 // function to create HTML file
-function writeToFile(fileName, arr) {
-  return fs.writeFileSync(path.join("./dist/roster.html", html), function (err) {
+const writeFile = data => {
+  fs.writeFile("./dist/roster.html", data, err => {
     if (err) {
       console.log(err);
+      return;
     } else {
       console.log("You successfully wrote to the file!");
     }
-  });
-};
-// initialize app 
-function init() {
-  prompt(addEmployee)
-  .then(answers => {
-    writeToFile('roster.html', genHTML(answers))
   })
 };
-};
-addEmployee();
-genHTML();
 
-module.exports = [employees];
+addEmployee()
+.then(employees => {
+  return genHtml(employees);
+})
+.then(html => {
+  return writeFile(html);
+})
+.catch(err => {
+  console.log(err);
+})
+// function writeToFile(fileName, employees, err) {
+//   // function (err) {
+//     console.log(employees)
+//     console.log()
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("You successfully wrote to the file!");
+//       fs.writeFileSync(createPath, genHtml(employees), "utf-8")
+//     }
+//   };
+// // initialize app 
+// function init() {
+//   addEmployee()
+//   .then(answers => {
+//     writeToFile("roster.html", genHtml(answers))
+//   })};
+// // addEmployee();
+// // genHTML();
+// init();
+// module.exports = [employees];
--- src/page-template.js
+++ src/page-template.js
@@ -1,15 +1,107 @@
 // creates html for employees like markdown....need a loop for # of employees, so need an array in other index.js
-// create Html
-function genHtml(data) {
+// create manager card
+const genMan = function (manager) {
+   return `
+     <div class="col-6 mt-6">
+              <div class="card mx-auto mb-3" style="width: 18rem">
+              <div class="card-header">
+              <h3>${manager.name}</h3>
+              <h4>Manager</h4>
+              </div>
+
+              <div class="card-body">
+              <p class="id">ID: ${manger.id}</p>
+              <p class="email">Email: <a href="email:${manager.email}">${manager.email}</a></p>
+              <p class="work">Work Number: ${manager.workNumber}</p>
+              </div>
+
+              </div>
+              </div>
+              `;
+}
+// create engineer card
+const genEng = function (engineer) {
     return `
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Team Gen</title>
+      <div class="col-6 mt-6">
+               <div class="card mx-auto mb-3" style="width: 18rem">
+               <div class="card-header">
+               <h3>${engineer.name}</h3>
+               <h4>Engineer</h4>
+               </div>
+ 
+               <div class="card-body">
+               <p class="id">ID: ${engineer.id}</p>
+               <p class="email">Email: <a href="email:${engineer.email}">${engineer.email}</a></p>
+               <p class="github">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+               </div>
+ 
+               </div>
+               </div>
+               `;
+ }
+ // create intern card
+const genIn = function (intern) {
+    return `
+      <div class="col-6 mt-6">
+               <div class="card mx-auto mb-3" style="width: 18rem">
+               <div class="card-header">
+               <h3>${intern.name}</h3>
+               <h4>Engineer</h4>
+               </div>
+ 
+               <div class="card-body">
+               <p class="id">ID: ${intern.id}</p>
+               <p class="email">Email: <a href="email:${intern.email}">${intern.email}</a></p>
+               <p class="school">School Name: ${intern.school}">${intern.school}</a></p>
+               </div>
+ 
+               </div>
+               </div>
+               `;
+ };
+
+ // push array
+ genHtml = (data) => {
+     employees = [];
+
+     for (let i = 0; i < data.length; i++) {
+         const employee = data[i];
+         const role = employee.getRole();
+
+         if (role === 'Manager') {
+             const managerCard = genMan(employee);
+             employees.push(managerCard);
+         }
+
+         if (role === 'Engineer') {
+            const engineerCard = genEng(employee);
+            employees.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = genIn(employee);
+            employees.push(internCard);
+        }
+     }
+
+     // combining strings
+     const employeeCards = employees.join('')
+
+     // return html
+     const genTeam = genHtml(employeeCards);
+     return genTeam;
+     
+     // generate html
+     const genHtml = function (employeeCards) {
+         return`
+         <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>Team Gen</title>
 </head>
 <body>
 <header>
@@ -25,7 +117,8 @@ function genHtml(data) {
     </div> 
 </body>
 </html>
-`;
-}
+`
+}}
 
+// export
 module.exports = genHtml;
